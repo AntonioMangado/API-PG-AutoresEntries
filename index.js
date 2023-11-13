b6b47ffe24b80c2c5b602eb4be3166ab25c00dbd
AntonioMangado <antonio.mangado@gmail.com>
2023-11-13 19:59:28 +0400

    ADD/ dotenv config

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
   // Variables globales
+  require("dotenv").config();
   const express = require('express')
   const app = express()
-  const port = 3000
+  const port = process.env.PORT || 3000;
   const cowsay = require("cowsay")
   
   // Middlewares
